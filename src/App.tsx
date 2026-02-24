9404e9e260a00ca3eef7e929c63a55bf451430ac
Muhammed Emin Cimen <muhammedemincmn@icloud.com>
2026-02-24 21:11:20 +0300

    chore: initial project setup with Vite React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Muhammed Emin Çimen</p>
+      <p>Ogrenci No: [235541088]</p>
+    </div>
+  )
+}
+
+export default App
