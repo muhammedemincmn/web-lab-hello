eac1bca1757bc3f1da0fbf7fec38b0802c3ee331
Muhammed Emin Cimen <muhammedemincmn@icloud.com>
2026-02-24 21:13:46 +0300

    style: update body background color to #f0f8ff

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
       <h1>Web Tasarimi ve Programlama</h1>
       <h2>LAB-1</h2>
       <p>Ad Soyad: Muhammed Emin Çimen</p>
-      <p>Ogrenci No: [235541088]</p>
+      <p>Ogrenci No: 235541088</p>
+      <h3>Hakkımda</h3>
+      <p>Yazılım mühendisliği öğrencisiyim. DevOps, bulut mimarileri (AWS) ve mobil uygulama geliştirme (iOS) alanlarında çalışmalar yapıyorum.</p>
     </div>
   )
 }
